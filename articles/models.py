9d71b357bf56086ac46cdaa87141ba909928dcfb
iamkryptonite <iamkryptonite@users.noreply.github.com>
2021-03-15 23:39:18 +0530

    added article model

---
+++ articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200,default="Enter title")
+    author = models.CharField(max_length=50,default="Enter author")
+    content = models.TextField(default="Add content")
+    date = models.DateField(auto_now_add=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.content[:50] + '...'
+
